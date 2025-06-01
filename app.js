f988c73753ebefafbe614580810528da1c3045e3
rich <richard.ewin@volvocars.com>
2025-06-01 19:51:23 +0200

    feat: add sorted and twoSum functions with corresponding tests

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ export function flatten(array) {
 
 	return array.reduce((acc, prevValue) => acc.concat(prevValue), []);
 }
+
+export function sorted(array) {
+	return array.sort((a, b) => a - b);
+}
+
+export function twoSum(array, target) {
+	for (let i = 0; i < array.length; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[i] + array[j] === target) {
+				return [i, j];
+			}
+		}
+	}
+}
--- app.test.js
+++ app.test.js
@@ -1,6 +1,17 @@
 import { expect, test } from "vitest";
-import { flatten } from "./app.js";
+import { flatten, plusOne, sorted, twoSum } from "./app.js";
 
 test("flatten", () => {
 	expect(flatten([1, 2, 3, [4, 5]])).toStrictEqual([1, 2, 3, 4, 5]);
 });
+
+test("sorted", () => {
+	const list = [5, 4, 3, 2, 1];
+	expect(sorted(list)).toBe(list.reverse());
+});
+
+test("twoSum", () => {
+	expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+  expect(twoSum([7, 3, 11, 15], 10)).toStrictEqual([0, 1]);
+});
+
